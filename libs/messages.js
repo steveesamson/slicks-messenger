85a8893033f9916722b823b3c968cb7a61bd1da7
Steve Samson <stevee.samson@gmail.com>
2019-10-27 14:37:04 +0100

    remove msgs

--- libs/messages.js
+++ libs/messages.js
@@ -1,7 +1,3 @@
 module.exports = {
-    'user.created': 'Hello {to_user_id}, welcome to konvaj.com! We congratulate you for this life-changing decision. Do visit the konvaj funding page to kick-start a rewarding fund raising. Again, welcome.',
-    'send.activation': 'Hi {to_user_id}, you are expected to pay N{amount_paid} to {partner_user_id} for your activation to level {stage} within the next {period} hours. Failure will put your account on hold.',
-    'resend.activation': 'Hi {to_user_id}, your payment of N{amount_paid} to {partner_user_id} for your activation to level {stage} has been rejected. You should resolve this within the next {period} hours. Failure will put your account on hold.',
-    'confirm.activation': 'Congratulations, {to_user_id}!. You have received activation payment of ${amount_paid} from {partner_user_id}, login to konvaj.com to confirm the receipt within the next {period} hours or it will be accepted on your behalf automatically.',
-    'otp.code': 'Hello {to_user_id}, you are about to recover your konvaj.com credentials. Use the code: {code} to activate your changes.'
+    
 };
